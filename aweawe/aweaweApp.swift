ee6a3f987d3afbc47a44f4125efeb10f785a7a2a
Weronika Nowak <weronika.nowak@pollub.edu.pl>
2023-10-03 14:37:56 +0200

    Initial Commit

---
+++ aweawe/aweaweApp.swift
@@ -0,0 +1,17 @@
+//
+//  aweaweApp.swift
+//  aweawe
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct aweaweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
